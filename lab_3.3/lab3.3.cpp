24e271332d60296568b2063506811b64e5fa134e
unknown <sashkocenyuk@gmail.com>
2025-09-30 21:32:04 +0300

    changes

--- lab_3.3/lab3.3.cpp
+++ lab_3.3/lab3.3.cpp
@@ -1,40 +1,48 @@
-// Lab_03_3.cpp 
-// Ценюк Олександр 
-// Лабораторна робота № 3.3 
-// Розгалуження, задане графіком функції. 
-// Варіант 26 
-#include <iostream> 
-#include <cmath> 
+// Lab_03_3.cpp
+// Ценюк Олександр
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 26
+
+#include <iostream>
+#include <cmath>
+#include <iomanip> 
 
 using namespace std;
 
 int main()
 {
-    double x;  // вхідний аргумент 
-    double R;  // вхідний параметр 
-    double y;  // результат обчислення виразу 
+    double x; 
+    double R;   
+    double y;
 
-    cout << "R = "; cin >> R;
-    cout << "x = "; cin >> x;
-
-    // розгалуження в повній формі 
-    if (x <= -5)
-        y = -3;
-    else 
-        if (-5 < x && x <= -R)
-            y = -3 + 3.0 / 5 - R * (x + 5);  // коригування поділу
-        else 
-            if (-R < x && x <= R)
-                y = sqrt(R * R - x * x);  // перевірка на негативне значення під коренем
-            else 
-                if (R < x && x < 8)
-                    y = R / 8 - R * (x - R);  // коригування для цього діапазону
-                else
-                    y = R;
-
+    cout << " Vvedit R: ";
+    cin >> R;
+    cout << "Vvedit x: ";
+    cin >> x;
+    if (x < -5)
+    {
+        y = -3.0;
+    }
+    else if (x <= -R)
+    {
+        y = (3.0 / (5.0 - R)) * (x + R);
+    }
+    else if (x < R)
+    {
+        y = sqrt(R * R - x * x);
+    }
+    else if (x <= 8)
+    {
+        y = (R / (8.0 - R)) * (x - R);
+    }
+    else
+    {
+        y = R;
+    }
     cout << endl;
+    cout << fixed << setprecision(4); // 4 знаки після коми вивід
     cout << "y = " << y << endl;
-
     cin.get();
     return 0;
 }
